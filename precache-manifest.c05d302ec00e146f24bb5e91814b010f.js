5ac0024d966a1dd29d5890f54dde92b325d80034
Dominika Hustinova <dominika.hustinova@kiwi.com>
2019-05-22 22:10:47 +0200

    Updates

---
+++ precache-manifest.c05d302ec00e146f24bb5e91814b010f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d41ab7747ba7d44014c904fafbb705f",
+    "url": "/orbit-portfolio/index.html"
+  },
+  {
+    "revision": "820b504344bbafb3c8c4",
+    "url": "/orbit-portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "049974971dd60358518d",
+    "url": "/orbit-portfolio/static/js/2.149ceffc.chunk.js"
+  },
+  {
+    "revision": "820b504344bbafb3c8c4",
+    "url": "/orbit-portfolio/static/js/main.6f6fdfd8.chunk.js"
+  },
+  {
+    "revision": "458bfc313c0e773b655a",
+    "url": "/orbit-portfolio/static/js/runtime~main.0adf0212.js"
+  }
+]);
