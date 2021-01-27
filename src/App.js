0c078e6a2e0ea43598167f830e1b9bd7813b02c2
domi <dominika.hustinova@kiwi.com>
2021-01-27 02:12:04 +0100

    refactor: remove unnecessary icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// import React framework
 import React from "react";
+
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
@@ -14,18 +14,6 @@ import { defaultTokens } from "@kiwicom/orbit-design-tokens";
 // import Orbit components
 // list of all components on: https://orbit.kiwi/components and https://kiwicom.github.io/orbit-components/?path=/story
 import Stack from "@kiwicom/orbit-components/lib/Stack";
-
-import Illustration from "@kiwicom/orbit-components/lib/Illustration";
-
-// import Orbit icons
-// list of all icons on: https://kiwicom.github.io/orbit-components/?path=/story/icon--list-of-all-icons
-import GenderWoman from "@kiwicom/orbit-components/lib/icons/GenderWoman";
-import Check from "@kiwicom/orbit-components/lib/icons/Check";
-import ChevronRight from "@kiwicom/orbit-components/lib/icons/ChevronRight";
-import ContactEmail from "@kiwicom/orbit-components/lib/icons/ContactEmail";
-import Entertainment from "@kiwicom/orbit-components/lib/icons/Entertainment";
-import Map from "@kiwicom/orbit-components/lib/icons/Map";
-import Phone from "@kiwicom/orbit-components/lib/icons/Phone";
 
 function App() {
   return (
